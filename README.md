6356f9d077d530b042f7fa83220aa0823ff8aeaa
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-10 18:33:22 +0200

    Let user to be the codemaker, make other improvements

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mastermind
-Mastermind or Master Mind is a code-breaking game for two players. In this Ruby version, the computer is a codemaker and the user guesses randomly generated code. Duplicates are allowed, so the computer could even choose four code pegs of the same color.
+Mastermind or Master Mind is a code-breaking game for two players. In the Ruby version, the user can choose to guess and make the computer the codemaker and randomly generate a code. Duplicates are allowed, so the computer could even choose four code pegs of the same color.
 
 The user tries to guess the pattern, in both order and color, within twelve turns. Each guess is made by typing the colors in the CLI. Once entered, the computer provides feedback by printing to the screen if the number of guesses that are right but in the wrong spot and how many is correct in both color and position. See [Wikipedia](https://en.wikipedia.org/wiki/Mastermind_(board_game)) for more information and game rules.
 
--- mastermind.rb
+++ mastermind.rb
@@ -1,89 +1,149 @@
 class Game
 
   def initialize
+    @colors = %w{blue green orange purple red black}
+    @turns_limit = 12
     @computer = Computer.new
     interface
   end
 
   def interface
     puts "Mastermind"
-    puts "\nColors in the game are #{@computer.colors[0..-1].join(', ')}."
-    puts "Computer generated the code..."
-    puts "You have 12 turns to guess the code."
-    @turns_limit = 12
-    begin
-      puts "Enter your guess:"
+    puts "\nColors in the game are #{@colors[0..-1].join(', ')}.\n\n"
+    puts "Would you like to be the codemaker or codebreaker?"
+    print ">"
+    answer = gets.strip.scan(/[a-zA-Z]+/)
+    # answer = "codemaker"    # For faster testing
+    if answer.include?("codebreaker") && !answer.include?("codemaker")
+      @computer.generate_code(@colors)
+      puts "You have #{@turns_limit} turns to guess the code."
+      begin
+        puts "Enter your guess:"
+        print ">"
+        user_input = gets.strip.scan(/[a-zA-Z]+/)
+        puts "\n"
+        puts "#" * 60
+        puts "\n"
+        if user_input.size == 4 && (user_input - @colors).size == 0
+          @turns_limit -= 1
+          victory = feedback(user_input, @computer.secret_code, "user")
+        elsif user_input.join == "help"
+          puts File.read('help.txt')
+        elsif user_input.join == "exit"
+          puts "Are you sure you want to leave the game?(y/n)"
+          answer = gets.strip
+          user_input = [] unless answer == "y"
+        else
+          puts "Wrong input!"
+        end
+      end until user_input.join == "exit" || victory || @turns_limit == 0
+    elsif answer.include?("codemaker") && !answer.include?("codebreaker")
+      puts "Enter your secret code: "
       print ">"
-      user_input = gets.strip.scan(/[a-zA-Z]+/)
-      puts "\n"
-      puts "#" * 60
-      puts "\n"
-      if user_input.size == 4 && (user_input - @computer.colors).size == 0
-        @turns_limit -= 1
-        victory = feedback(user_input)
-      elsif user_input.join == "help"
+      users_secret_code = gets.strip.scan(/[a-zA-Z]+/)
+      # users_secret_code = %w{blue red black green}    # For faster testing
+      if users_secret_code.size == 4 && (users_secret_code - @colors).size == 0
+        puts "Computer has #{@turns_limit} turns to guess the code.\n\n"
+        begin
+          @computer.generate_guess(@colors)
+          @turns_limit -= 1
+          puts "Computer's guess is | #{@computer.guess_code.join(' | ')} |"
+          victory = feedback(@computer.guess_code, users_secret_code, "computer")
+          puts "\n"
+          puts "#" * 60
+          puts "\n"
+        end until victory || @turns_limit == 0
+      elsif users_secret_code.join == "help"
         puts File.read('help.txt')
-      elsif user_input.join == "exit"
-        puts "Are you sure you want to leave the game?(y/n)"
-        answer = gets.strip
-        user_input = [] unless answer == "y"
       else
         puts "Wrong input!"
       end
-    end until user_input.join == "exit" || victory == true || @turns_limit == 0
+    else
+      puts "I don't understand that."
+    end
   end
 
-  def feedback(input)
+private
+
+  def feedback(guess_code, secret_code, codebreaker)
     correct_counter = 0
     color_match_counter = 0
     color_catcher = Hash.new(0)
-    input.each { |color| color_catcher[color] += 1 }
-    input.each_with_index do |color, index|
-      if @computer.secret_code[index] == input[index]
+    guess_code.each { |color| color_catcher[color] += 1 }
+    guess_code.each_with_index do |color, index|
+      if secret_code[index] == guess_code[index]
         correct_counter += 1
-      elsif @computer.secret_code.include?(color) && color_catcher[color] < 2
+      elsif secret_code.include?(color) && color_catcher[color] < 2
         color_match_counter += 1
       end
     end
     puts "Correct color, and in the right hole: #{correct_counter}"
     puts "Correct color, but in the wrong hole: #{color_match_counter}"
-    you_win(@computer.secret_code, correct_counter)
+    victory?(correct_counter, secret_code, codebreaker)
   end
 
-  def you_win(code, correct_counter)
-    if correct_counter == 4
-      puts "You guessed the code!"
-      puts "The code is | #{code.join(" | ")} |"
-      return true
-    else
-      if @turns_limit > 0
-        puts "#{@turns_limit - 1} more turns left."
+  def victory?(correct_counter, secret_code, codebreaker)
+    if codebreaker == "user"
+      if correct_counter == 4
+        puts "You guessed the code!"
+        puts "The code is | #{code.join(" | ")} |"
+        true
+      elsif @turns_limit > 0
+        puts "#{@turns_limit} more turns left."
         puts "Try again..."
+        false
       elsif @turns_limit == 0
         puts "You ran out of limit in 12 guesses..."
-        puts "Computer's code was | #{code.join(" | ")} |"
+        puts "Computer's code was | #{secret_code.join(" | ")} |"
+        true
+      end
+    elsif codebreaker == "computer"
+      if correct_counter == 4
+        puts "Computer guessed the code!"
+        puts "The code is | #{code.join(" | ")} |"
+        true
+      elsif @turns_limit > 0
+        puts "#{@turns_limit} more turns left."
+        puts "Computer is trying again..."
+        false
+      elsif @turns_limit == 0
+        puts "Computer ran out of limit in 12 guesses..."
+        puts "Your secret code was | #{secret_code.join(" | ")} |"
+        true
       end
     end
   end
 end
 
 class Computer
-  attr_reader :secret_code, :colors
+  attr_reader :secret_code, :guess_code
 
   def initialize
-    @colors = %w{blue green orange purple red black}
-    generate_code
+    # puts "Computer is ready..." # For fun sake :-D
+    @secret_code = []
   end
 
-  def generate_code
-    @secret_code = []
-
-    4.times do |i|
+  def generate_code(colors)
+    4.times do
       @secret_code << rand(6)
     end
     @secret_code.each_with_index do |item, index|
-      @secret_code[index] = @colors[item]
+      @secret_code[index] = colors[item]
     end
+    puts "Computer generated the code..."
+  end
+
+  def generate_guess(colors)
+    @guess_code = []
+    puts "Computer makes a guess..."
+    sleep 3
+    4.times do
+      @guess_code << rand(6)
+    end
+    @guess_code.each_with_index do |item, index|
+      @guess_code[index] = colors[item]
+    end
+    @guess_code
   end
 end
 
